19b0ae4d3c6648617421dbee3592a1493f774f9c
sandhya Jagtap <sandhyajagtap05022000@gmail.com>
2024-10-25 13:07:25 +0530

    String Faist Test Case Pass

--- src/main/java/org/example/Calculator/stringCalculator.java
+++ src/main/java/org/example/Calculator/stringCalculator.java
@@ -2,6 +2,9 @@ package org.example.Calculator;
 
 public class stringCalculator {
 
+public String toUpperCase(String s){
+    return s.toUpperCase();
+}
 
 
 }
--- src/test/java/org/example/Calculator/stringCalculatorJava.java
+++ src/test/java/org/example/Calculator/stringCalculatorJava.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
-import static org.example.Calculator.stringCalculator.isvalidsname;
+//import static org.example.Calculator.stringCalculator.isvalidsname;
 //import static org.junit.jupiter.api.AssertTrue.assertTrue;
 
 public class stringCalculatorJava {
